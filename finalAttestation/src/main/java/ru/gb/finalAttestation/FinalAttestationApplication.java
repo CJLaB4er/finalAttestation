7806575f729bfb07af6b3b1b40b89799ef377790
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-07-08 22:22:49 +0300

    init commit

---
+++ finalAttestation/src/main/java/ru/gb/finalAttestation/FinalAttestationApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.finalAttestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAttestationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAttestationApplication.class, args);
+	}
+
+}
---
+++ finalAttestation/src/test/java/ru/gb/finalAttestation/FinalAttestationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.finalAttestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAttestationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
